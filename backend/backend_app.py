ab4acfbbe8debf19bbf51b35252f93d78ccc0d1b
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-24 12:58:39 +0100

    Added endpoint to add a blog post with some input validation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)
 
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
@@ -12,7 +12,20 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """ Gets all blog posts in JSON Format."""
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_posts():
+    """ Adds a blog post to the JSON list of posts."""
+    new_post = request.get_json()
+    if not new_post.get('title') or not new_post.get('content'):
+        return jsonify({"Error": "Title and Content are required!"}), 400
+
+    new_post['id'] = len(POSTS) + 1
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
